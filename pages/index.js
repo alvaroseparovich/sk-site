99b95679051ffda5e643368223fde98b7423f739
alvaroseparovich <alvasepro@gmail.com>
2020-02-24 21:04:55 -0300

    TWEAK: code calling change

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import mainTemplate from '../template'
-import {BRAND_NAME} from '../brand-conf'
+import Row from '../template/matComponents/row'
 
-const text = `
+const TrabalheNaAlemanhaText = `
+  Somos diferente das outras agencias, não você não precisa se preocupar procurando vagas.
 
-`
+  Agilisamos todo o processo pra você.
+  Nos informe suas qualificações, e faremos o trabalho sujo por você!
+  `
+
+const TrabalheNaAlemanha = function () {
+  return Row({InnerH:()=>(<>
+    <br/>
+    <h4 className="card-title">
+      Trabalhe na Alemanha! nós te ajudamos.
+    </h4>
+    <p>{TrabalheNaAlemanhaText}</p>
+    <br/>
+    <ul className="col s6 left-align">
+      <li>Ajudamos na Documentação</li>
+      <li>Procuramos uma boa empresa</li>
+      <li>Te conectamos com diversas empresas</li>
+      <li>Te ajudamos no processo seletivo</li>
+      <li>Você começa a trabalhar na Alemanha!</li>
+    </ul>
+    <img className="col s6" src="/pngs/floating-to-germany-compressd.png" alt="voando para a alemanha"/>
+    <br/>
+  </>
+  )})
+  }
 
 export default () => mainTemplate(<>
-  <div className="row blue darken-1 center white-text"> <br/>
-    <div className="container">
-        <br/>
-        <h4 className="card-title">
-          Porque escolher a {BRAND_NAME}?
-        </h4>
-        <p>porque nao cobramos bsta nenhuma</p>
-        <br/>
-        <img src="/pngs/floating-to-germany-compressd.png" alt=""/>
-    </div>
-  </div>
+<TrabalheNaAlemanha/>
 </>)
